ea03f2dd52091a0187fa1da8fc8ec65948b0455c
fujifuji031 <106134395+fujifuji031@users.noreply.github.com>
2022-08-07 14:49:54 +0900

    再レンダリングの最適化

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { ChildArea } from "./ChildrenArea";
 import "./styles.css";
 
@@ -6,6 +6,13 @@ export default function App() {
   const [text, setText] = useState("");
   const [open, setOpen] = useState(false);
 
+  //アロー関数で書くと毎回新しい関数を生成している→処理は同じでも渡すpropsが変化している
+  //useCallbackを使うことで、第2引数に設定された変数に変更があったときだけ新しく関数を生成する処理に変えられる
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  //useMemoを使うことで変数のmemo化が可能(新しい変数を変数を生成しない)
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
   const onChangeText = (event) => setText(event.target.value);
   const onClickOpen = () => setOpen(!open);
   return (
@@ -14,7 +21,7 @@ export default function App() {
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
--- src/ChildrenArea.jsx
+++ src/ChildrenArea.jsx
@@ -8,14 +8,21 @@ const style = {
 
 //memoで囲むことでpropsの値に変更がない限り、childrenAreaは更新されない
 export const ChildArea = memo((props) => {
-  const { open } = props;
+  const { open, onClickClose } = props;
   console.log("ChildrenAreaが再レンダリング");
+
+  //2000個データを用意
+  const data = [...Array(2000).keys()];
+  //2000回中の処理を繰り返す
+  data.forEach(() => {
+    console.log("...");
+  });
   return (
     <>
       {open ? (
         <div style={style}>
           <p>子コンポーネント</p>
-          <button>閉じる</button>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
